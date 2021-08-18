4de44acc6dd677aea12bd9e9a4578a7d52e47168
Sh11ntar0 <sh11ntar0h@gmail.com>
2021-08-17 19:58:44 -0700

    modd name

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <SHeroSection>
       <SHero>
         <STitle>Hello,</STitle>
-        <STitle>&nbsp;I'm DAIKI</STitle>
+        <STitle>&nbsp;I'm Shintaro</STitle>
         <SSubtitle>A Front-end</SSubtitle>
         <SSubtitle>&nbsp;Web Developer</SSubtitle>
       </SHero>
